02fa6e99be29f40ad6e6c89082e56b1eec02c9d5
shreyas <oyedrizzle@gmail.com>
2026-05-25 16:27:27 +0530

    Create 2015-DetermineWhetherMatrixCanBeObtainedByRotation

---
+++ 2015-DetermineWhetherMatrixCanBeObtainedByRotation/2015-DetermineWhetherMatrixCanBeObtainedByRotation.java
@@ -0,0 +1,44 @@
+// Last updated: 5/25/2026, 4:27:21 PM
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            if (isEqual(mat, target)) return true;
+            rotate(mat);
+        }
+        return false;
+    }
+
+    private void rotate(int[][] mat) {
+        int n = mat.length;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    private boolean isEqual(int[][] a, int[][] b) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (a[i][j] != b[i][j]) return false;
+            }
+        }
+        return true;
+    }
+}
